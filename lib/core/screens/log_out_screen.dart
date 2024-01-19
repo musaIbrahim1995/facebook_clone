b73a61b23e9ea633c8bdb3e44c7f134e7f28e957
musaIbrahim1995 <ibrahimxtrimious@gmail.com>
2024-01-19 02:25:15 -0800

    first commit

---
+++ lib/core/screens/log_out_screen.dart
@@ -0,0 +1,109 @@
+//import 'package:facebook_clone/features/chat/presentation/screens/chat_screen.dart';
+import 'package:facebook_clone/features/auth/providers/auth_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '/core/constants/app_colors.dart';
+import '/core/constants/constants.dart';
+import '/core/constants/extensions.dart';
+import '/core/screens/error_screen.dart';
+import '/core/screens/loader.dart';
+import '/core/widgets/round_button.dart';
+import '/features/auth/providers/get_user_info_as_stream_by_id_provider.dart';
+//import '/features/friends/presentation/widgets/add_friend_button.dart';
+import '/features/posts/presentation/widgets/icon_text_button.dart';
+
+class LogoutScreen extends ConsumerWidget {
+  const LogoutScreen({
+    super.key,
+    this.userId,
+  });
+
+  final String? userId;
+
+  static const routeName = '/logout';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final myUid = FirebaseAuth.instance.currentUser!.uid;
+    final uid = userId ?? myUid;
+    final userInfo = ref.watch(getUserInfoAsStreamByIdProvider(uid));
+
+    return userInfo.when(
+      data: (user) {
+        return SafeArea(
+          child: Scaffold(
+            appBar: userId != myUid
+                ? AppBar(
+                    title: const Text('Logout Screen'),
+                  )
+                : null,
+            backgroundColor: AppColors.whiteColor,
+            body: Padding(
+              padding: Constants.defaultPadding,
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(user.profilePicUrl),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    user.fullName,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 21,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  RoundButton(
+                      onPressed: () {
+                        ref.read(authProvider).signOut();
+                      },
+                      label: 'Log Out'),
+                  const SizedBox(height: 20),
+                  _buildProfileInfo(
+                    email: user.email,
+                    gender: user.gender,
+                    birthday: user.birthDay,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return ErrorScreen(error: error.toString());
+      },
+      loading: () {
+        return const Loader();
+      },
+    );
+  }
+
+  _buildProfileInfo({
+    required String email,
+    required String gender,
+    required DateTime birthday,
+  }) =>
+      Column(
+        children: [
+          IconTextButton(
+            icon: gender == 'male' ? Icons.male : Icons.female,
+            label: gender,
+          ),
+          const SizedBox(height: 10),
+          IconTextButton(
+            icon: Icons.cake,
+            label: birthday.yMMMEd(),
+          ),
+          const SizedBox(height: 10),
+          IconTextButton(
+            icon: Icons.email,
+            label: email,
+          ),
+        ],
+      );
+}
